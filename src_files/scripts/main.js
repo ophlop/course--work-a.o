1b7817de92b8c37bad70fb6453956c7c154cfaec
Oleg Astashin <95183292+ophlop@users.noreply.github.com>
2023-01-14 14:27:26 +0400

    - fuck gitHub and his synhronic files

--- src_files/scripts/main.js
+++ src_files/scripts/main.js
@@ -1,54 +1,40 @@
-// Currency API script part begin
+// Currency API script part
 let coine = "USD";
 let coineTwo = "RUB";
 let json;
 
-function showActualCourses(currencyPairsPrice = [{ from: "USD", to: "RUB" }]) {
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "34eb46cf27msh7166df608a6238cp161b5ejsn0f643a99bbf7",
-      "X-RapidAPI-Host": "currency-exchange.p.rapidapi.com",
-    },
-  };
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "34eb46cf27msh7166df608a6238cp161b5ejsn0f643a99bbf7",
+    "X-RapidAPI-Host": "currency-exchange.p.rapidapi.com",
+  },
+};
+let currencyPairsPrice = ["USD", "EUR", "CNY", "CHF", "JPY", "TRY"];
 
-  let requests = currencyPairsPrice.map((el) =>
+let renderDIVParent = document.querySelector(".left-row__course-info");
+
+for (let i = 0; i < currencyPairsPrice.length; i++) {
+  let renderDIVChild = document.createElement("div");
+  let span1 = document.createElement("span");
+  let span2 = document.createElement("span");
+  renderDIVChild.classList.add("left-row__course-card");
+  span1.classList.add("course-card__currencies");
+  span2.classList.add("course-card__price-course");
+  span1.innerHTML = `${currencyPairsPrice[i]}:`;
+  span2.innerHTML = `${setInterval(function cileCurrency() {
     fetch(
-      `https://currency-exchange.p.rapidapi.com/exchange?from=${el}&to=RUB&q=1.0`,
+      `https://currency-exchange.p.rapidapi.com/exchange?from=${currencyPairsPrice[i]}&to=${coineTwo}&q=1.0`,
       options
     )
       .then((response) => {
-        if (!response.ok) {
-          console.log("response1", response);
-          throw new Error("Fetch error");
-        }
-        console.log("response2", response);
         return response.json();
       })
-      .then((response) => {
-        el.course = response.toFixed(2);
-        console.log("response3", response.json());
-      })
-  );
-  return requests;
-  Promise.all(requests)
-    .then(() => {
-      if (!showLeftRowInfo) {
-        createCourses(currencyPairsPrice);
-        console.log("CC", createCourses(currencyPairsPrice));
-      } else {
-        updateCourses(currencyPairsPrice);
-        console.log("else CC", updateCourses(currencyPairsPrice));
-      }
-    })
-    .catch((err) => {
-      console.log(`${err}`);
-    });
+      .then((data) => {
+        span2.innerHTML = `${data.toFixed(2)}`;
+      });
+  }, 1500)}}`;
+  renderDIVChild.appendChild(span1);
+  renderDIVChild.appendChild(span2);
+  renderDIVParent.appendChild(renderDIVChild);
 }
-
-showActualCourses(currencyPairsPrice);
-setInterval(() => {
-  showActualCourses(currencyPairsPrice);
-}, 900000);
-
-// Currency API script part end
