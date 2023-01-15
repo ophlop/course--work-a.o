6a7179d08d99464098642fd85dc60393449c20ee
Oleg Astashin <95183292+ophlop@users.noreply.github.com>
2023-01-16 01:15:27 +0400

    - margin update

--- src_files/scripts/main.js
+++ src_files/scripts/main.js
@@ -14,15 +14,44 @@ let currencyPairsPrice = ["USD", "EUR", "CNY", "CHF", "JPY", "TRY"];
 
 let renderDIVParent = document.querySelector(".left-row__course-info");
 
-for (let i = 0; i < currencyPairsPrice.length; i++) {
-  let renderDIVChild = document.createElement("div");
-  let span1 = document.createElement("span");
-  let span2 = document.createElement("span");
-  renderDIVChild.classList.add("left-row__course-card");
-  span1.classList.add("course-card__currencies");
-  span2.classList.add("course-card__price-course");
-  span1.innerHTML = `${currencyPairsPrice[i]}:`;
-  span2.innerHTML = `${setInterval(function cileCurrency() {
+let oldSpan = [];
+
+function firstValue() {
+  for (let i = 0; i < currencyPairsPrice.length; i++) {
+    let renderDIVChild = document.createElement("div");
+    let span1 = document.createElement("span");
+    let span2 = document.createElement("span");
+    renderDIVChild.classList.add("left-row__course-card");
+    span1.classList.add("course-card__currencies");
+    span2.classList.add("course-card__price-course");
+    span2.classList.add(`currencyCell${i}`);
+    span1.innerHTML = `${currencyPairsPrice[i]}:`;
+    span2.innerHTML = `${fetch(
+      `https://currency-exchange.p.rapidapi.com/exchange?from=${currencyPairsPrice[i]}&to=${coineTwo}&q=1.0`,
+      options
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        oldSpan.push(`${data.toFixed(2)}`);
+        console.log(oldSpan);
+        span2.innerHTML = `${data.toFixed(2)}`;
+      })}`;
+    renderDIVChild.appendChild(span1);
+    renderDIVChild.appendChild(span2);
+    renderDIVParent.appendChild(renderDIVChild);
+  }
+  return oldSpan;
+}
+firstValue(oldSpan);
+
+console.log(oldSpan);
+console.log(oldSpan.length);
+
+function newCourses() {
+  let newCourses = [];
+  for (let i = 0; i < currencyPairsPrice.length; i++) {
     fetch(
       `https://currency-exchange.p.rapidapi.com/exchange?from=${currencyPairsPrice[i]}&to=${coineTwo}&q=1.0`,
       options
@@ -31,10 +60,25 @@ for (let i = 0; i < currencyPairsPrice.length; i++) {
         return response.json();
       })
       .then((data) => {
-        span2.innerHTML = `${data.toFixed(2)}`;
+        newCourses.push(`${data.toFixed(2)}`);
       });
-  }, 1500)}}`;
-  renderDIVChild.appendChild(span1);
-  renderDIVChild.appendChild(span2);
-  renderDIVParent.appendChild(renderDIVChild);
+  }
+  return newCourses;
 }
+
+let updateSpan = newCourses();
+console.log(updateSpan);
+console.log(updateSpan.length);
+
+function cicleUpdateCourse(oldSpan, updateSpan) {
+  for (let i = 0; i < currencyPairsPrice.length; i++) {
+    if (oldSpan[i] !== updateSpan[i]) {
+      let newValue = document.querySelector(`.currencyCell${i}`);
+      newValue.innerHTML = updateSpan[i];
+    }
+  }
+}
+
+setInterval(newCourses(), 5000);
+
+setInterval(cicleUpdateCourse(), 5000);
