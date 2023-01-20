cdbf01b403f0312287a87d06e98119abab22fd05
Astashin Oleg <astashinoleg@gmail.com>
2023-01-20 16:22:55 +0300

    - start project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import LoanPage from './pages/LoanPage';
+import MainPage from './pages/MainPage';
 
 
 function App() {
   return (
     <div>
-      Hello World!
+      <Routes>
+        <Route path='/' element={ <MainPage /> }></Route>
+        <Route path='loan' element={ <LoanPage /> }></Route>
+      </Routes>
     </div>
   );
 }
