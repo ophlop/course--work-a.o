83a09a8d24f1e48133164f9a4696636be16e95dd
Astashin Oleg <astashinoleg@gmail.com>
2023-01-20 16:23:35 +0300

    - start working

---
+++ src/pages/LoanPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LoanPage(): JSX.Element {
+  return (
+    <div>LoanPage</div>
+  )
+}
+
+export default LoanPage();
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MainPage(): JSX.Element {
+  return (
+    <div>MainPage</div>
+  )
+}
+
+export default MainPage();
