e9ee2ff486c88335e9a053f1c07b41bb323e41d4
Astashin Oleg <astashinoleg@gmail.com>
2023-01-24 16:32:59 +0300

    - redux setup

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { setupStore } from './store';
+
+
+const store = setupStore()
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  <BrowserRouter>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    </BrowserRouter>
+  </Provider>
 );
